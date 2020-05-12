41b2379f14b0e60f531de689bae7685e9ef8176a
Jorge Pabon <jmpabon@gmail.com>
2020-05-12 13:55:49 +0800

    MainActivity renamed: CountMeInActivity

--- app/src/main/java/jpabon/com/countmein/CountMeInActivity.kt
+++ app/src/main/java/jpabon/com/countmein/CountMeInActivity.kt
@@ -3,10 +3,10 @@ package jpabon.com.countmein
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CountMeInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_count_me_in)
     }
 }
