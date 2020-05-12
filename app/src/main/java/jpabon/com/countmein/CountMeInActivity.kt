29f3a1f282a55b94cea9feb6f083ad7378be43ef
Jorge Pabon <jmpabon@gmail.com>
2020-05-12 17:53:13 +0800

    Search button action added
    On click action for search button added to uodate result message to "No events found" (6 of 6 test passed)

--- app/src/main/java/jpabon/com/countmein/CountMeInActivity.kt
+++ app/src/main/java/jpabon/com/countmein/CountMeInActivity.kt
@@ -2,11 +2,20 @@ package jpabon.com.countmein
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import jpabon.com.countmein.databinding.ActivityCountMeInBinding
 
 class CountMeInActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityCountMeInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_count_me_in)
+        binding = ActivityCountMeInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSearchEvent.setOnClickListener {
+            binding.tvSearchResultMessage.text = "No events found"
+        }
     }
 }
