f4df9f1dfac1a970420e11773be9b72330d9f126
Jorge Pabon <jmpabon@gmail.com>
2020-08-23 19:53:25 +0800

    Navigation controller and tool bar initialization on main activity

--- app/src/main/java/jpabon/com/countmein/ui/CountMeInActivity.kt
+++ app/src/main/java/jpabon/com/countmein/ui/CountMeInActivity.kt
@@ -2,7 +2,12 @@ package jpabon.com.countmein.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import jpabon.com.countmein.R
 import jpabon.com.countmein.databinding.ActivityCountMeInBinding
 
 @AndroidEntryPoint
@@ -12,9 +17,17 @@ class CountMeInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityCountMeInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
+        val navController: NavController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
